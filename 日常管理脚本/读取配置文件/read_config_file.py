c1da4b56bf7fadd26cf879947536e54f5416cd6c
jwh5566 <jwh5566@foxmail.com>
2020-01-23 15:32:08 +0800

    读取配置文件

---
+++ 日常管理脚本/读取配置文件/read_config_file.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Time : 2020/1/23 15:24
+# @Author : jwh5566
+# @Email : jwh5566@aliyun.com
+# @File : read_config_file.py
+
+from configparser import ConfigParser
+
+p = ConfigParser()
+p.read('read_simple.ini')
+print(p.get('bug_tracker', 'url'))
---
+++ 日常管理脚本/读取配置文件/read_many_config_file.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# @Time : 2020/1/23 15:26
+# @Author : jwh5566
+# @Email : jwh5566@aliyun.com
+# @File : read_many_config_file.py
+
+from configparser import ConfigParser
+
+p = ConfigParser()
+files = ['hello.ini', 'bye.ini', 'read_simple.ini', 'welcome.ini']
+files_found = p.read(files)
+
+files_missing = set(files) - set(files_found)
+
+print('找到文件:', sorted(files_found))
+print('丢失文件:', sorted(files_missing))
